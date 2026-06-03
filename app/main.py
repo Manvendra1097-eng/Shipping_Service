3e80a93b3e22120e20e051bd8ac1386415a1bf0d
Manvendra <69745291+Manvendra1097-eng@users.noreply.github.com>
2026-06-03 08:36:52 +0000

    added path variable code

--- app/main.py
+++ app/main.py
@@ -1,14 +1,34 @@
 from fastapi import FastAPI
 from scalar_fastapi import get_scalar_api_reference
+from typing import Any
 
 app = FastAPI()
 
-@app.get("/shipment")
-def get_shipment():
-    return {
-        "content":"wooden table",
-        "status": "in tansit"
+shipments = {
+    12701:{
+        "id": 12701,
+        "content": "Wooden table",
+        "status": "in-transit"
+    },
+     12702:{
+        "id": 12702,
+        "content": "Wooden Chai",
+        "status": "Ordered"
     }
+}
+
+@app.get("/shipment/latest")
+def get_latest_shipment() -> dict[str,Any]:
+    id = max(shipments.keys())
+    return shipments[id]
+
+@app.get("/shipment/{id}")
+def get_shipment(id: str) -> dict[str, Any]:
+    if id not in shipments:
+        return {
+            "details": "Given ID doesn't exits"
+        }
+    return shipments[id]
 
 @app.get("/scalar",include_in_schema=False)
 def scalar_html():
